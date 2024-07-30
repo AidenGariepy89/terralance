3a428b25d4fc2e2f0eb388c6d00310c1dda5c69b
Aiden Gariepy <aidengariepy89@gmail.com>
2024-07-29 20:45:24 -0400

    feat: hsv to rgb

--- src/main.zig
+++ src/main.zig
@@ -2,41 +2,10 @@ const std = @import("std");
 const math = @import("math.zig");
 const w = @import("window.zig");
 const rl = @import("raylib");
-const assert = @import("debug.zig").assert;
+const assert = @import("assert.zig").assert;
 const print = std.debug.print;
 
 pub fn main() !void {
-    const seed: u64 = @intCast(std.time.milliTimestamp());
-    const noise = math.PerlinNoise.init(seed);
-
-    const size = 200;
-    const fidelity: f32 = 0.005;
-
-    var smallest: f32 = 100;
-    var largest: f32 = -100;
-
-    const threshold = 0.4999;
-    var tiles: [size * size]bool = undefined;
-    // var colors: [size * size]u8 = undefined;
-    for (0..size) |i| {
-        for (0..size) |j| {
-            const x: f32 = @as(f32, @floatFromInt(i)) * fidelity;
-            const y: f32 = @as(f32, @floatFromInt(j)) * fidelity;
-            const noise_val = noise.fbm(x, y, 5) * 0.5 + 0.5;
-            tiles[(i * size) + j] = noise_val > threshold;
-
-            if (noise_val > largest) {
-                largest = noise_val;
-            }
-            if (noise_val < smallest) {
-                smallest = noise_val;
-            }
-        }
-    }
-
-    print("smallest: {d}\n", .{smallest});
-    print("largest: {d}\n", .{largest});
-
     w.init(800, 600);
 
     while (!w.shouldClose()) {
@@ -44,20 +13,6 @@ pub fn main() !void {
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
-
-        const width = 1;
-        for (0..size) |i| {
-            for (0..size) |j| {
-                // const col = colors[(i * size) + j];
-                // rl.drawRectangle(@intCast(i * width), @intCast(j * width), width, width, rl.Color.init(col, col, col, 255));
-                const green = tiles[(i * size) + j];
-                if (green) {
-                    rl.drawRectangle(@intCast(i * width), @intCast(j * width), width, width, rl.Color.green);
-                } else {
-                    rl.drawRectangle(@intCast(i * width), @intCast(j * width), width, width, rl.Color.blue);
-                }
-            }
-        }
     }
 
     defer w.deinit();
--- src/math.zig
+++ src/math.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const t = std.testing;
-const assert = @import("debug.zig").assert;
+const assert = @import("assert.zig").assert;
 
 pub const pi = std.math.pi;
 
@@ -61,7 +61,10 @@ pub const Vec = struct {
     }
 
     pub fn norm(self: Vec) Vec {
-        const scalar = 1.0 / self.mag();
+        const m = self.mag();
+        assert(m != 0, "Can't normalize a vector with no length!");
+
+        const scalar = 1.0 / m;
         return self.scale(scalar);
     }
 };
@@ -71,12 +74,8 @@ pub const PerlinNoise = struct {
     const Self = @This();
 
     permutations: [512]u8,
-    seed: u64,
 
-    pub fn init(seed: u64) Self {
-        var rng = std.Random.DefaultPrng.init(seed);
-        const random = rng.random();
-
+    pub fn init(random: std.Random) Self {
         var perms: [256]u8 = undefined;
         for (0..256) |i| {
             perms[i] = @intCast(i);
@@ -96,10 +95,7 @@ pub const PerlinNoise = struct {
             p[i] = perms[i % 256];
         }
 
-        return .{
-            .permutations = p,
-            .seed = seed,
-        };
+        return .{ .permutations = p };
     }
 
     /// Fractal Brownian Motion
@@ -178,7 +174,44 @@ pub const PerlinNoise = struct {
 };
 
 pub fn lerp(a1: f32, a2: f32, percent: f32) f32 {
+    assert(percent >= 0 and percent <= 1, "Percent must be >= 0 and <= 1");
     return a1 + percent * (a2 - a1);
+}
+
+pub const HSV = struct {
+    /// In degrees, 0 <= hue <= 360
+    hue: f32,
+    saturation: f32,
+    value: f32,
+};
+
+pub const RGB = struct {
+    r: u8,
+    g: u8,
+    b: u8,
+};
+
+pub fn hsv_to_rgb(hsv: HSV) RGB {
+    assert(hsv.hue >= 0 and hsv.hue <= 360 , "Hue is in degrees and must be >= 0 and <= 360");
+    assert(hsv.saturation >= 0 and hsv.saturation <= 1, "Saturation must be >= 0 and <= 1");
+    assert(hsv.value >= 0 and hsv.value <= 1, "Value must be >= 0 and <= 1");
+
+    const r_f = hsv_to_rgb_transformation(hsv, 5);
+    const g_f = hsv_to_rgb_transformation(hsv, 3);
+    const b_f = hsv_to_rgb_transformation(hsv, 1);
+
+    return RGB{
+        .r = @intFromFloat(@round(r_f * 255)),
+        .g = @intFromFloat(@round(g_f * 255)),
+        .b = @intFromFloat(@round(b_f * 255)),
+    };
+}
+
+fn hsv_to_rgb_transformation(hsv: HSV, n: f32) f32 {
+    const k: f32 = @mod((n + hsv.hue / 60), 6);
+    const result: f32 = hsv.value - hsv.value * hsv.saturation * @max(0, @min(k, @min(4 - k, 1)));
+    assert(result >= 0 and result <= 1, "Result must be >= 0 and <= 1");
+    return result;
 }
 
 test "dot and cross" {
@@ -190,4 +223,34 @@ test "dot and cross" {
 
     const dot = vec_a.dot(vec_b);
     try t.expectEqual(4, dot);
+}
+
+test "hsv to rgb" {
+    const input_red = HSV{
+        .hue = 0,
+        .saturation = 1,
+        .value = 1,
+    };
+    const expected_red = RGB{
+        .r = 255,
+        .g = 0,
+        .b = 0,
+    };
+
+    const actual_red = hsv_to_rgb(input_red);
+    try t.expectEqual(expected_red, actual_red);
+
+    const input_purple = HSV{
+        .hue = 283,
+        .saturation = 0.49,
+        .value = 0.92,
+    };
+    const expected_purple = RGB{
+        .r = 202,
+        .g = 120,
+        .b = 235,
+    };
+
+    const actual_purple = hsv_to_rgb(input_purple);
+    try t.expectEqual(expected_purple, actual_purple);
 }
